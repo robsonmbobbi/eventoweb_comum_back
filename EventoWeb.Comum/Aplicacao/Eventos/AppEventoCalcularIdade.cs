119c0db643aaf4e38f401f39a49f1e3e38934a92
Robson <robson.bobbi@realclub.com.br>
2026-02-17 18:20:13 -0300

    Continuação

---
+++ EventoWeb.Comum/Aplicacao/Eventos/AppEventoCalcularIdade.cs
@@ -0,0 +1,29 @@
+﻿using EventoWeb.Comum.Negocio.ObjetosValor;
+using EventoWeb.Comum.Negocio.Repositorios;
+
+namespace EventoWeb.Comum.Aplicacao.Eventos
+{
+    public class AppEventoCalcularIdade : AppBase
+    {
+        private readonly IEventos m_Eventos;
+
+        public AppEventoCalcularIdade(IContexto contexto, IEventos eventos) : base(contexto)
+        {
+            m_Eventos = eventos;
+        }
+
+        public int CalcularIdade(int idEvento, DateTime dataNascimento)
+        {
+           int idade = 0;
+            ExecutarSeguramente(() =>
+            {
+                var evento = m_Eventos.Obter(idEvento) ?? throw new Exception("Evento não encontrado.");
+
+                idade = DataAniversario.ObterIdadeAnos(dataNascimento, evento.PeriodoRealizacaoEvento.DataInicial);
+
+            });
+
+        return idade;
+        }
+    }
+}
